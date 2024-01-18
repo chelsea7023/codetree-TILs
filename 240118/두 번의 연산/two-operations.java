da8a14386aff1a0cb3aa5ff23f536a3f649e7d84
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-01-18 22:35:26 +0900

    Title: 두 번의 연산, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/두 번의 연산/two-operations.java
+++ 240118/두 번의 연산/two-operations.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a%3==0){
+        if(a%2!=0){
             a+=3;
         }if(a%3==0){
             a/=3;
