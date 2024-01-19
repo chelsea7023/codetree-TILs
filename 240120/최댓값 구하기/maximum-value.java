3787f3e7ab2566d26c15e1df02f27c8cab238ef8
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-01-20 00:15:36 +0900

    Title: 최댓값 구하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240120/최댓값 구하기/maximum-value.java
+++ 240120/최댓값 구하기/maximum-value.java
@@ -7,9 +7,9 @@ public class Main {
         a= sc.nextInt();
         b= sc.nextInt();
         c= sc.nextInt();
-        if(a>=b&&b>=c)
+        if(a>=b&&a>=c)
             System.out.println(a);
-        else if(b>=a&&a>=c)
+        else if(b>=a&&b>=c)
             System.out.println(b);
         else
             System.out.println(c);
