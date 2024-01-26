3c1c4ce4807aa7c66511085b5a55c42266df7d7b
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-01-27 01:49:22 +0900

    Title: a의 b승, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240127/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        int x = 1;
+        for(int i = 1; i<=b; i++){
+            x*=a;
+        }System.out.println(x);
+    }
+}
