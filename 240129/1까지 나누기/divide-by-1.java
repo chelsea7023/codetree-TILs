2d8af9737570f469af8edd127b627c85e163a9b5
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-01-29 01:48:47 +0900

    Title: 1까지 나누기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240129/1까지 나누기/divide-by-1.java
+++ 240129/1까지 나누기/divide-by-1.java
@@ -5,10 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; true; i++){
-            if(n>i){
-                n=n/i;
+            n=n/i;
+            if(n>1){
+                
                 continue;
-            }else if(n<=i) {
+            }else {
                 System.out.println(i);
                 break;
                 
