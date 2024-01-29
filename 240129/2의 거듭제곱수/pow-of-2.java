2e84615247d2518092edc57e9e9c97c5bd08e012
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-01-29 17:25:32 +0900

    Title: 2의 거듭제곱수, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true){
+            if(n>1){
+                n/=2;
+                cnt ++;
+            }else if(n==1){
+                System.out.println(cnt);
+                break;
+            }
+        }
+    }
+}
