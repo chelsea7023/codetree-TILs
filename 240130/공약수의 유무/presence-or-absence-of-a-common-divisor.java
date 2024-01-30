cead0bb2ee02bb957cd5c0f1752b49e2c7a9e296
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-01-30 10:50:52 +0900

    Title: 공약수의 유무, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 240130/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -10,8 +10,8 @@ public class Main {
         for(int i = a; i <= b; i++){
             if(1920 % i == 0 && 2880 % i == 0){
                 satisfied = true;
-            } 
-        }if(satisfied = true){
+            }
+        }if(satisfied == true){
             System.out.println(1);
         }else {
             System.out.println(0);
