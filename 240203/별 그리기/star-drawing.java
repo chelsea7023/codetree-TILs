fc8cb7fa272d09a969f672b3f7a67e4a2e856576
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-03 18:32:05 +0900

    Title: 별 그리기, Time: 312ms, Memory: 14MB, Status: Passed - CodeTree

--- 240203/별 그리기/star-drawing.java
+++ 240203/별 그리기/star-drawing.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         for(int i = 0; i < n; i++){
             for(int j = n - (i + 1); j > 0; j--){
-                System.out.print("  ");
+                System.out.print(" ");
             }
             for(int j = 0; j < (2 * i) + 1; j++){
                 System.out.print('*');
@@ -14,7 +14,7 @@ public class Main {
         }
         for(int i = 0; i < n - 1; i++){
             for(int j = 0; j < i+1; j++){
-                System.out.print("  "); 
+                System.out.print(" "); 
             }
             for(int j = (2 * n) - (2 * i) - 3; j > 0; j--){
                 System.out.print('*');
