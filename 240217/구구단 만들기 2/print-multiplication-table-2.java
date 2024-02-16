9924201e2c8ba9ca1cebd8b64a7f61a20e1641c0
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-17 02:11:40 +0900

    Title: 구구단 만들기 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240217/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        for(int i = 2; i <= 8; i+=2){
+            for(int j = b; j >= a; j--){
+                System.out.print(j + " * " + i + " = " + j*i);
+                if(j <= b && j > a){
+                    System.out.print(" / ");
+                }
+            }System.out.println();
+        }
+    }
+}
