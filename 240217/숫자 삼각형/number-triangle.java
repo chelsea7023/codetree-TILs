e2af16d0ef9e0c9937d55563a4571c38642a5cd5
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-17 02:14:54 +0900

    Title: 숫자 삼각형, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240217/숫자 삼각형/number-triangle.java
+++ 240217/숫자 삼각형/number-triangle.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < N; i++){
             for(int j = 0; j < i + 1; j++){
                 System.out.print((j+1) + " ");
             }System.out.println();
