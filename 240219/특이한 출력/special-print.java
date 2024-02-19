1c3823003fa16b17b29165f8da5589317b2ea8fd
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-19 19:28:38 +0900

    Title: 특이한 출력, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240219/특이한 출력/special-print.java
+++ 240219/특이한 출력/special-print.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                if(i+j ==4){
+                if((i+j)%4 == 0){
                     System.out.print("(" + i + ", " + j + ") " );
                     System.out.println();
                 }else {
