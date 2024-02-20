5253c9bcacec3ff9fa6edebc2b6f6ba46c979ef7
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-21 00:20:43 +0900

    Title: 특정 숫자 도달하기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240221/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[10];
+        for(int i = 0; i < 10; i ++){
+            num[i] = sc.nextInt();
+        }
+        int sum = 0; 
+        int sum2= 0;
+        int cnt = 0;
+        int cnt2 = 0;
+        for(int i = 0; i < 10; i++){
+            if(num[i] >= 250){
+                for(int j = 0; j < i; j++){
+                    sum += num[j];
+                    cnt++;
+                }System.out.printf(sum + " " + "%.1f", ((double)sum/cnt));
+                break;
+            }
+        }
+    
+}
+}
