035965e64703dd073d1b389bdf977fde15447a69
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-21 20:42:24 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240221/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int cnt = 0;
+        for(int i = 0; i < T; i++){
+            int[] arr = new int[4];
+            int sum = 0; 
+            for(int j = 0; j < 4; j++){
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+            }
+             
+            if((sum/4)>=60){
+                System.out.println("pass");
+                cnt++;
+            }else{
+                System.out.println("fail");
+            }
+            
+        }System.out.println(cnt);
+    }
+}
