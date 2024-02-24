c1538fb252cff6625ca14808dd87d95d93c5d095
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-25 00:16:10 +0900

    Title: 100 도달하기, Time: 133ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240225/100 도달하기/reach-100.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[10];
+        arr[0] = 1;
+        arr[1] = n;
+        int cnt = 0;
+        for(int i = 2; i < 10; i++){
+            arr[i] = arr[i - 1] + arr[i - 2];
+            if(arr[i] > 100){
+                cnt = i;
+                break;
+                
+            }
+        }
+        for(int i = 0; i <= cnt; i++){
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
