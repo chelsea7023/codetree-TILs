025c3c87d981893c03b4b7dcd50dac48d57827cc
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-25 00:16:26 +0900

    Title: 100 도달하기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240225/100 도달하기/reach-100.java
+++ 240225/100 도달하기/reach-100.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[10];
+        int[] arr = new int[12];
         arr[0] = 1;
         arr[1] = n;
         int cnt = 0;
