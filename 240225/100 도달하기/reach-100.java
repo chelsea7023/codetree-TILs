c910d5579b10ffa64862105cc370157ec0d92301
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-25 00:17:40 +0900

    Title: 100 도달하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240225/100 도달하기/reach-100.java
+++ 240225/100 도달하기/reach-100.java
@@ -8,7 +8,7 @@ public class Main {
         arr[0] = 1;
         arr[1] = n;
         int cnt = 0;
-        for(int i = 2; i < 10; i++){
+        for(int i = 2; i < 12; i++){
             arr[i] = arr[i - 1] + arr[i - 2];
             if(arr[i] > 100){
                 cnt = i;
