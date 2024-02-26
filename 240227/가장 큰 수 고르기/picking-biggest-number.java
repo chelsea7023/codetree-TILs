077d66d3accb7b9d311a20b26feb27ed6c6ec77f
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-02-27 01:03:20 +0900

    Title: 가장 큰 수 고르기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240227/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+
+    }
+}
