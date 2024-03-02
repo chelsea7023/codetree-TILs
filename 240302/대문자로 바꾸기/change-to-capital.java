bc8e432555f96daeb829bc5af94408371b844e30
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-02 22:26:41 +0900

    Title: 대문자로 바꾸기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240302/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[][] word = new String[5][3];
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 3; j++){
+                word[i][j] = sc.next();
+            }
+        }
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(word[i][j].toUpperCase()+ " ");
+            }System.out.println();
+        }
+    }
+}
