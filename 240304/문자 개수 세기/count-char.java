732d2c6e5f462f57a868697c80d01d03d59cc702
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-04 17:00:08 +0900

    Title: 문자 개수 세기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240304/문자 개수 세기/count-char.java
+++ 240304/문자 개수 세기/count-char.java
@@ -4,9 +4,11 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        // sc.nextLine();
+        char a = sc.next().charAt(0);
         int cnt = 0;
         for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == 'e'){
+            if(str.charAt(i) == a){
                 cnt++;
             }
         }
