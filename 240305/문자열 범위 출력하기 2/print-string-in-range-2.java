1279907d799745cd99e8d0ce93970df16813389a
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-05 23:47:49 +0900

    Title: 문자열 범위 출력하기 2, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240305/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 240305/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -5,8 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int num = sc.nextInt();
-        for(int i = str.length() - 1; i > str.length() - 12; i--){
+        if(num > str.length()){
+            for(int i = str.length() - 1; i >= 0; i--){
             System.out.print(str.charAt(i));
         }
+        }else{
+ for(int i = str.length() - 1; i > str.length() - 12; i--){
+            System.out.print(str.charAt(i));
+        }
+        }
+       
     }
 }
