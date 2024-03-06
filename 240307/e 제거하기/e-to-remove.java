b21e383ea39ab23ad029ec5168bd2d5b5cc260c6
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-07 01:58:49 +0900

    Title: e 제거하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240307/e 제거하기/e-to-remove.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int num = str.indexOf('e');
+        // System.out.println(num);
+        str = str.substring(0, 3) + str.substring(4);
+        System.out.println(str);
+    }
+}
