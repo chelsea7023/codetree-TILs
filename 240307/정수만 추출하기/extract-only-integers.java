3494c323a98e66ea8e3d613414f82dbe81f3d903
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-07 03:23:17 +0900

    Title: 정수만 추출하기, Time: 192ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/정수만 추출하기/extract-only-integers.java
+++ 240307/정수만 추출하기/extract-only-integers.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         String str2 = sc.next();
-        int num1 = 0;
+        int num1 = str.length();
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) < 48 || str.charAt(i) > 57){
                 num1 = i;
@@ -13,7 +13,7 @@ public class Main {
             }
         }
         str = str.substring(0, num1);
-        int num2 = 0;
+        int num2 = str2.length();
         for(int i = 0; i < str2.length(); i++){
             if(str2.charAt(i) < 48 || str2.charAt(i) > 57){
                 num2 = i;
