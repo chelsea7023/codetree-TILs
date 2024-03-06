22fd91d8d38442a9ceece189d6e03825d2ab616d
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-07 03:19:31 +0900

    Title: 정수만 추출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240307/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String str2 = sc.next();
+        int num1 = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) < 48 || str.charAt(i) > 57){
+                num1 = i;
+                break;
+            }
+        }
+        str = str.substring(0, num1);
+        int num2 = 0;
+        for(int i = 0; i < str2.length(); i++){
+            if(str2.charAt(i) < 48 || str2.charAt(i) > 57){
+                num2 = i;
+                break;
+            }
+        }
+        str2 = str2.substring(0, num2);
+        System.out.println(Integer.parseInt(str) + Integer.parseInt(str2));
+
+    }
+}
