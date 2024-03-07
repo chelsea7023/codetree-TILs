494154b915fac6629778a0f34fff476b54b5a1c4
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-03-08 01:38:55 +0900

    Title: 몫 출력하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240308/몫 출력하기/print-share.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        for(int i = 0; true; i++){
+            int a = sc.nextInt();
+            if(a % 2 == 0){
+                cnt++;
+                System.out.println(a/2);
+                if(cnt == 3){
+                    break;
+                }
+                
+            }else{
+                continue;
+            }
+        }
+    }
+}
